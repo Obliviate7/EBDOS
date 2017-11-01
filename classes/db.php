2f40f16a111897cc1f105a67c86e583fa210d87c
fabiancrux <fabiancrux@hotmail.com>
2017-11-01 13:37:13 -0300

    json sumado 1ra parte
    json sumado 1ra parte

--- classes/db.php
+++ classes/db.php
@@ -2,7 +2,7 @@
   require_once("user.php");
   abstract class db {
     public abstract function saveUser(User $email);
-    public abstract function getAll();
+    public abstract function getAll(); 
     public abstract function getByEmail($email);
     public abstract function modifyUser(User $email);
   }
--- classes/dbJSON.php
+++ classes/dbJSON.php
@@ -3,11 +3,51 @@
   require_once("db.php");
   class dbJSON extends db {
     private $file;
-    public function saveUser(User $email) {
-      $fp = fopen("users.json", "a+");
-      $result = fwrite($fp, $email . PHP_EOL);
+    public function saveUser(User $usuario) {
+      $pass = sha1($usuario->getPassword());
+      $jsonUser = json_encode([
+        'usrname'      => $usuario->getUsrName(),
+        'usrsurname'   => $usuario->getUsrSurname(),
+        'birthDate' => $usuario->getBirthDate(),
+        'email'     => $usuario->getEmail(),
+        'country' =>  $usuario->getCountry();
+        'province' =>  $usuario->getProvince();
+        'cit' =>  $usuario->getCity();
+        'zipcode' =>  $usuario->getZipCode();
+        'mobile' =>  $usuario->getMobile();
+        'address' =>  $usuario->getAddress();
+        'webpage' => $usuario->getWebPage();
+        'bio' =>  $usuario->getBio();
+        'password'  => $pass
+      ]);
+      if (writeUserFile($jsonUser)) {
+        uploadPhoto($photo);
+      }
       return $result;
     }
+
+    public function writeUserFile($jsonUser2) {
+      $fp = fopen("users.json", "a+");
+      $result = fwrite($fp, $jsonUser2 . PHP_EOL);
+      return $result;
+    }
+
+    public function uploadPhoto($photo){
+      if (count($photo)) {
+          $avatarFileName = $photo['name'];
+          $avatarFile = $photo['tmp_name'];
+          $avatarExtension = pathinfo($avatarFileName, PATHINFO_EXTENSION);
+          $hash= sha1($user['username']);
+          $result = move_uploaded_file($avatarFile, 'avatars/' . $hash . '.' . $avatarExtension);
+          $arch= $hash.".".$avatarExtension;
+           return $arch;
+      }
+      else {
+        return "";
+      }
+    }
+
+
     public function getAll() {
       $fp = fopen('users.json', 'r');
       $allDbJSON = json_decode(fgets($fp), true);
@@ -25,5 +65,7 @@
       }
       return false;
     }
+      public function modifyUser(User $usuario){
+        }
   }
 ?>
